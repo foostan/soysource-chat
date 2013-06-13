3bfb5f31bb2d5511881753428efaec3974fdba85
foostan <foostan27@gmail.com>
2013-06-14 01:43:04 +0900

    edit views

--- config/assets.js
+++ config/assets.js
@@ -5,11 +5,9 @@ module.exports.assets = {
 	// and then can be automatically injected in your layout/views via the view partials:
 	// ( assets.css(), assets.js() and assets.templateLibrary() )
 	sequence: [
-		'assets/js/vendor/jquery', 
-		'assets/js/vendor/bootstrap', 
-		'assets/js/soysource', 
 		'assets/mixins', 
 		'assets/styles', 
+		'assets/js', 
 		'assets/templates'
 	]
 };
